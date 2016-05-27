4d43c6b7a550910378ca3a00e6ee990ee67e1c89
mdyoyo <ht9938@qq.com>
2016-05-27 21:38:26 +0800

    1

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/',function(req,res){
+    res.sendFile('index.html');
+});
+
+io.on('connection',function(socket){
+    console.log('a user connected');
+    socket.on('chat message',function(msg){
+        console.log('message: '+msg);
+        io.emit('chat message',msg);
+    });
+});
+
+app.set('port',process.env.PORT || 9902);
+
+var server =  http.listen(app.get('port'),function(){
+    console.log('start at port:' + server.address().port);
+});
