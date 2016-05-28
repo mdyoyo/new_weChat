d6f79976321bf88085e39fd767887ccda42da4d9
mdyoyo <ht9938@qq.com>
2016-05-28 22:20:12 +0800

    1

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/',function(req,res){
-    res.sendFile('index.html');
+    res.sendFile('index2.html');
 });
 
 io.on('connection',function(socket){
     console.log('a user connected');
+    var clients = io.sockets.client();
+    console.log(clients);
+    console.log(io.sockets.connected);
     socket.on('chat message',function(msg){
         console.log('message: '+msg);
         io.emit('chat message',msg);
